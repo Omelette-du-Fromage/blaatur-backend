d882bcc93c876cd6cbe03a26b2db7d7e29f8ba1f
Zylvian <33404765+Zylvian@users.noreply.github.com>
2021-03-01 13:08:28 +0100

    deployman

--- flaskbackend/backman.py
+++ flaskbackend/backman.py
@@ -5,15 +5,18 @@ from flask_cors import CORS, cross_origin
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-#cors = CORS(app)
+cors = CORS(app)
 #app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
-@cross_origin(supports_credentials = True)
+#@cross_origin(supports_credentials = True)
 @app.route('/testing', methods=['GET'])
 def data():
     json_return = [{"start":"Voss", "end": "Bergen"}]
-    return jsonify(json_return)
+    response = jsonify(json_return)
+    # Enable Access-Control-Allow-Origin
+    response.headers.add("Access-Control-Allow-Origin", "*")
+    return response
