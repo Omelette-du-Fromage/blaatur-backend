cf2fca9b3129e6f97a9ddc8c56bf6c319b7ab123
Zylvian <33404765+Zylvian@users.noreply.github.com>
2021-03-01 13:38:30 +0100

    updated testing

---
+++ tests/test_api.py
@@ -0,0 +1,10 @@
+from flaskbackend.backman import app
+from flask import request, jsonify
+
+
+def test_api_start():
+    app.testing = True
+    with app.test_client() as c:
+        rv = c.get('/testing')
+        json_data = rv.get_json()
+        assert json_data[0]['start'] is not None
