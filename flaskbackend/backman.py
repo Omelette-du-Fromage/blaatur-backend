6c035eedec11eb94509c62b7f1dc9a4d59310143
Zylvian <33404765+Zylvian@users.noreply.github.com>
2021-03-01 12:43:34 +0100

    cors testing

--- flaskbackend/backman.py
+++ flaskbackend/backman.py
@@ -1,13 +1,18 @@
 import flask
 from flask import jsonify
+from flask_cors import CORS, cross_origin
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+#cors = CORS(app)
+#app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/', methods=['GET'])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
+@cross_origin(supports_credentials = True)
 @app.route('/testing', methods=['GET'])
 def data():
     json_return = [{"start":"Voss", "end": "Bergen"}]
