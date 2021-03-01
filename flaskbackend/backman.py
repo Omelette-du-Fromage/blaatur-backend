7b916210ece5c9c3ac5b6d15d3cd0097121cb0ca
Zylvian <33404765+Zylvian@users.noreply.github.com>
2021-03-01 13:15:10 +0100

    fixed flask cors

--- flaskbackend/backman.py
+++ flaskbackend/backman.py
@@ -18,5 +18,5 @@ def data():
     json_return = [{"start":"Voss", "end": "Bergen"}]
     response = jsonify(json_return)
     # Enable Access-Control-Allow-Origin
-    response.headers.add("Access-Control-Allow-Origin", "*")
+    #.headers.add("Access-Control-Allow-Origin", "*")
     return response
