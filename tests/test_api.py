cb92e1039692861f7a7f6891061979ae36d6f9e5
JennStro <jennys1994@gmail.com>
2021-03-02 13:18:31 +0100

    Added test fir aoi

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,4 +6,10 @@ def test_api_start():
     with app.test_client() as c:
         rv = c.get('/testing')
         json_data = rv.get_json()
-        assert json_data[0]['start'] is None
+        assert json_data[0]['start'] is not None
+
+def test_api_can_get_start_value():
+    app.testing = True
+    with app.test_client() as client:
+        req = client.get('/start?start=hei')
+        assert request.args.get('start') is not None
