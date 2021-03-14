803a70b94e07d6ad101af9aeeb316f408760a934
JennStro <jennys1994@gmail.com>
2021-03-14 16:49:02 +0100

    Remove unnecessary print

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,5 +4,4 @@ def test_api_start():
     app.testing = True
     with app.test_client() as c:
         rv = c.post("/testing",  json={"place_from": "Bergen"}, headers={"Content-Type": "application/json"})
-        print(rv)
         assert rv is not None
