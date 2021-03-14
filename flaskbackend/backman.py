a846084fb362f15be8dcbb73018dc2345b20737b
JennStro <jennys1994@gmail.com>
2021-03-14 16:24:03 +0100

    Check that place_to and place_from is not the same

--- flaskbackend/backman.py
+++ flaskbackend/backman.py
@@ -13,6 +13,7 @@ app.config["DEBUG"] = True
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
 @app.route("/", methods=["GET"])
 def home():
     return "<h1>Distant Reading Archive</h1><p>This site is a prototype API for distant reading of science fiction novels.</p>"
@@ -21,11 +22,11 @@ def home():
 @app.route("/testing", methods=["POST"])
 def data():
     # gets the "place" value from the HTTP body
-    places_to = ["Bergen", "Florø", "Arendal", "Voss", "Indre Arna", "Asker"]
-    place_to = random.choice(places_to)
-
     data = request.get_json()
     place_from = data.get("place_from", '')
+
+    places_to = ["Bergen", "Florø", "Arendal", "Voss", "Indre Arna", "Asker"]
+    place_to = findRandomPlaceTo(place_from, places_to)
 
     id_and_station_name_place_from = entur_api.place_getter(place_from)
     id_and_station_name_place_to = entur_api.place_getter(place_to)
@@ -38,9 +39,17 @@ def data():
         return "Record not found", 400
 
 
-
 @app.route("/start", methods=["GET"])
 def startingPoint():
     start = request.args.get("start")
     print(start)
     return jsonify([{"start": start}])
+
+
+def findRandomPlaceTo(place_from, places_to_go):
+    """Find a random place to go from list. If place_to and place_from is equal, find a new place."""
+    place_to_candidate = random.choice(places_to_go)
+    if place_to_candidate == place_from:
+        findRandomPlaceTo(place_from, places_to_go)
+    else:
+        return place_to_candidate
